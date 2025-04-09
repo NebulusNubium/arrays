20ec948f6b8fec84852e0e06c9830f4158b65de1
Théo <t.knopfer@gmail.com>
2025-04-09 11:34:19 +0200

    exo_4

--- exo_1.js
+++ exo_1.js
@@ -1,8 +1,4 @@
 let numbers = [1,2,3,4]
-
-const mult = numbers*2
-console.log(numbers[])
-
 for(let item of numbers) {
     console.log(item*2)
 }
---
+++ exo_3.js
@@ -0,0 +1,4 @@
+let fruits = ["apple", "banane", "cherry"]
+for(let item of fruits) {
+    console.log(item)
+}
